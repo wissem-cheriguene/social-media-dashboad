16448037b534c8ced3ac271902b095ec77e9d6a8
Wiss69 <wissem.cheriguene@gmail.com>
2020-04-12 23:59:04 +0200

    Add JS theme switcher + CSS variables to handle switching colors

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,24 @@
 var app = {
   init: function() {
     console.log('init');
+
+    const toogleButton= document.querySelector('.toogle-button > a');
+    console.log(toogleButton);
+
+    toogleButton.addEventListener('click', app.handleSwitchTheme);
+  },
+
+  handleSwitchTheme: function(e) {
+    console.log(e.target);
+    const fontAwesomeIcon = e.target;
+    fontAwesomeIcon.classList.toggle("fa-toggle-on");
+    fontAwesomeIcon.classList.toggle("fa-toggle-off");
+    if(fontAwesomeIcon.classList.contains('fa-toggle-on')) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
   }
 };
 
